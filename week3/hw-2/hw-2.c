3d30724adcc104b6285ef0296a66b438c5637209
eliteabhi <abhirangarajan2003@gmail.com>
2024-09-13 18:12:42 -0500

    Hw 1 Fixed and Lab 2 PDF

--- week3/hw-2/hw-2.c
+++ week3/hw-2/hw-2.c
@@ -1,53 +1,58 @@
+// Abhi Rangarajan uxs876
 #include <stdio.h>
 #include <stdlib.h> // For exit()
 
-int main(int argc, char** argv) {
-    FILE *from_ptr, *to_ptr;
+int main( int argc, char **argv ) {
+
+    FILE *from_ptr, *to1_ptr, *to2_ptr;
+    char *from, *to1, *to2;
     int c;
 
-    char* from = argv[0];
+    if ( argc < 4 ) {
 
-    // Open file to read
+        printf( "Usage: multiple_copies source_file destination_file1 destination_file2\n" );
+        exit(1);
+
+    }
+
+    from = argv[1];
+    to1 = argv[2];
+    to2 = argv[3];
+
+    // Open one file for reading
     from_ptr = fopen( from, "r" );
-    if ( from_ptr == NULL ) {
-
-        printf( "Cannot open file %s\n", from );
+    if ( from_ptr == NULL )
+    {
+        printf( "Opening Error!: %s\n", from );
         exit(1);
-
     }
 
-    if ( argc < 3 ) {
-
-        printf("No file to copy to\n");
+    // Open another file for writing
+    to1_ptr = fopen( to1, "w" );
+    if ( to1_ptr == NULL )
+    {
+        printf( "Opening Error!: %s\n", to1 );
         exit(1);
-
+    }
+    
+    // Open another file for writing
+    to2_ptr = fopen( to2, "w" );
+    if ( to2_ptr == NULL )
+    {
+        printf( "Opening Error!: %s\n", to2 );
+        exit(1);
     }
 
-    for ( int i = 1; i < argc; i++ ) {
-
-        char* to = argv[i];
-
-        scanf("%s", to);
-
-        // Open another file for writing
-        to_ptr = fopen( to, "w" );
-        if (to_ptr == NULL)
-        {
-            printf( "Cannot open file %s to write\n", to );
-            exit(1);
-        }
-
-        // Read contents from file
-        while ( ( c = fgetc(from_ptr) ) != EOF )
-        {
-            fputc( c, to_ptr );
-        }
-
-        fclose( to_ptr );
-
+    // Read contents from file
+    while ( ( c = fgetc( from_ptr ) ) != EOF )
+    {
+        fputc( c, to1_ptr );
+        fputc( c, to2_ptr );
     }
 
     fclose( from_ptr );
-
+    fclose( to1_ptr );
+    fclose( to2_ptr );
     return 0;
+
 }
