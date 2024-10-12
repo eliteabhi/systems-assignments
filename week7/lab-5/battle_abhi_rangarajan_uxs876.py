0b6ea7154adbad8527e7f8e18fa7392b7da302b9
eliteabhi <abhirangarajan2003@gmail.com>
2024-10-12 18:30:13 -0500

    Lab 5 with a heap

--- week7/lab-5/battle_abhi_rangarajan_uxs876.py
+++ week7/lab-5/battle_abhi_rangarajan_uxs876.py
@@ -1,22 +1,27 @@
 from typing import List
+import heapq as hq
 
 def battle( monsters: List[ int ] ) -> None:
+
+    # Dirty way to convert to a Max heap
+    monsters = list( map( lambda x: x * -1, monsters ) )
+
+    hq.heapify( monsters )
+
+    print( monsters )
 
     while len( monsters ) > 1:
         monster1: int = monsters[0]
         monster2: int = monsters[1]
 
-        if monster1 > monster2:
-            monsters[0] -= monster2
-            monsters.pop( 1 )
+        if monster1 == monster2:
+            hq.heappop( monsters )
+            hq.heappop( monsters )
+            continue
 
-        elif monster2 > monster1:
-            monsters[1] -= monster1
-            monsters.pop( 0 )
+        push_item: int = monster1 - monster2
 
-        else:
-            monsters.pop( 0 )
-            monsters.pop( 0 )
+        hq.heappushpop( monsters, push_item )
 
     if monsters:
         print('I have won, but at what cost?')
@@ -30,7 +35,11 @@ def battle( monsters: List[ int ] ) -> None:
 Test1 = [ 4, 4, 1 ]
 Test2 = [ 1, 1, 1, 1, 1, 1 ]
 Test3 = [ 9, 8, 7, 6 ]
+Test4 = [ 12, 10, 9, 9, 8, 9 ]
+
 
 battle( Test1 )
 battle( Test2 )
 battle( Test3 )
+battle( Test4 )
+
